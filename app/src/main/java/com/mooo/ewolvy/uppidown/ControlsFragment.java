84bdd5b19c6fea2dd5ce72ff248867ea86f777da
Juanjo Soriano <juanjosoriano@gmail.com>
2017-04-24 17:35:19 +0200

    Minor update on name of AA

--- app/src/main/java/com/mooo/ewolvy/uppidown/ControlsFragment.java
+++ app/src/main/java/com/mooo/ewolvy/uppidown/ControlsFragment.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
